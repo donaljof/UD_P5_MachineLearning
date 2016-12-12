385042aaf4a68e9664d8bc66f6dd936c86c15ed0
donaljof <donal.ofarrell@gmail.com>
2016-12-12 21:25:07 +0000

    outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 def outlierCleaner(predictions, ages, net_worths):
     """
         Clean away the 10% of points that have the largest
@@ -14,7 +13,17 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
-
+    #creating a series of tuples for the prediction-output pairs
+    predictions = zip(ages, net_worths, predictions)
+    data = []
+    for i in predictions:
+        error = i[1] - i[2]
+        tup = (i[0], i[1], error)
+        data.append(tup)
+        
+    sorted_data = sorted(data, key=lambda point: point[2])
+    cleaned_data = sorted_data[ len(data)/10:]
     
+
     return cleaned_data
 
