546e2aec0bae1851204e3475fc21c6eb55526739
donaljof <donal.ofarrell@gmail.com>
2017-01-08 19:08:24 +0000

    updated_textlearning_featureselection

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -4,9 +4,10 @@ import os
 import pickle
 import re
 import sys
-
 sys.path.append( "../tools/" )
+
 from parse_out_email_text import parseOutText
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 """
     Starter code to process the emails from Sara and Chris to extract
@@ -39,29 +40,45 @@ temp_counter = 0
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
+        
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
+        #temp_counter += 1
+        if temp_counter < 5:
+            path = path[:-2] + "_"
+            path = os.path.join('...', path) 
+            #path = 'C:/Users/Donal/Documents/Udacity Docs/UD_P5_MachineLearning/maildir/bailey-s/deleted_items/101_'
+                   
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            text = parseOutText(email)
             ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
-
+            ### 
+            key_words = ["sara", "shackleton", "chris", "germani"]
+            for kword in key_words:
+                text = text.replace(kword, '')
+        
             ### append the text to word_data
-
+            word_data.append(text)
+            
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
+            if name == "sara":
+                from_data.append(0)
+            elif name == "chris":
+                from_data.append(1)
+            else:
+                print "unidentified from name"
+                
+                
             email.close()
+            
 
 print "emails processed"
 from_sara.close()
 from_chris.close()
+
+#print "udacity quiz = ", word_data[152] 
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
@@ -72,4 +89,8 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 ### in Part 4, do TfIdf vectorization here
 
+vectorizer = TfidfVectorizer(stop_words='english', lowercase = True)
 
+vectorizer.fit_transform(word_data)
+
+print vectorizer.get_feature_names()[34597]
